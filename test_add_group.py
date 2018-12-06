db7dd8591085f49cacba81c435e2c755072bdd01
elenaerdman <lenauttwin@gmail.com>
2018-12-06 21:06:45 +0300

    refactor parameters group

--- test_add_group.py
+++ test_add_group.py
@@ -27,19 +27,19 @@ class TestADDGroup(unittest.TestCase):
     def open_groups_page(self, wd):
         wd.find_element_by_link_text("groups").click()
 
-    def create_group(self, wd):
+    def create_group(self, wd, name, header, footer):
         # unit group creation
         wd.find_element_by_name("new").click()
         # fill group form
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("qwerty1")
+        wd.find_element_by_name("group_name").send_keys(name)
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("qwerty1")
+        wd.find_element_by_name("group_header").send_keys(header)
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("qwerty1")
+        wd.find_element_by_name("group_footer").send_keys(footer)
         # submit
         wd.find_element_by_name("submit").click()
 
@@ -54,7 +54,7 @@ class TestADDGroup(unittest.TestCase):
         self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
         self.open_groups_page(wd)
-        self.create_group(wd)
+        self.create_group(wd, name="qwerty1", header="qwerty1", footer="qwerty1")
         self.return_to_groups_page(wd)
         self.logout(wd)
 
