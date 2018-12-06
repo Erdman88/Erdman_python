ea968d55e49c467aca75e10a31ac6964903b3b86
elenaerdman <lenauttwin@gmail.com>
2018-12-06 21:10:02 +0300

    add metod test_add_empty_group

--- test_add_group.py
+++ test_add_group.py
@@ -49,7 +49,7 @@ class TestADDGroup(unittest.TestCase):
     def logout(self, wd):
         wd.find_element_by_link_text("Logout").click()
 
-    def test_a_d_d_group(self):
+    def test_add_group(self):
         wd = self.wd
         self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
@@ -58,6 +58,14 @@ class TestADDGroup(unittest.TestCase):
         self.return_to_groups_page(wd)
         self.logout(wd)
 
+    def test_add_empty_group(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, username="admin", password="secret")
+        self.open_groups_page(wd)
+        self.create_group(wd, name="", header="", footer="")
+        self.return_to_groups_page(wd)
+        self.logout(wd)
 
     def tearDown(self):
         self.wd.quit()
