f969161511880d4e2d2eaab112d674ed8bd01cfe
elenaerdman <lenauttwin@gmail.com>
2018-12-06 21:17:54 +0300

    optimise

--- test_add_group.py
+++ test_add_group.py
@@ -1,12 +1,8 @@
 # -*- coding: utf-8 -*-
+import unittest
+
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.firefox.webdriver import WebDriver
-from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
-import unittest, time, re
+
 
 class TestADDGroup(unittest.TestCase):
     def setUp(self):
