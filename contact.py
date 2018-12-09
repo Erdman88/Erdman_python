c5ea0bfd21aaec1379e4870e8a71feec595bfe2e
elenaerdman <lenauttwin@gmail.com>
2018-12-09 22:54:47 +0300

    класс Contact

---
+++ contact.py
@@ -0,0 +1,28 @@
+
+class Contact:
+
+      def _init_(self, firstname, middlename, lastname, nickname, title, company, address, hometelephone,
+                       mobiletelephone, worktelephone, faxtelephone, email, homepage, bday, bmonth, byear, aday, amonth,
+                       ayear, address2, phone2, notes):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nickname = nickname
+        self.title = title
+        self.company = company
+        self.address = address
+        self.hometelephone = hometelephone
+        self.mobiletelephone = mobiletelephone
+        self.worktelephone = worktelephone
+        self.faxtelephone = faxtelephone
+        self.email = email
+        self.homepage = homepage
+        self.bday = bday
+        self.bmonth = bmonth
+        self.byear = byear
+        self.aday = aday
+        self.amonth = amonth
+        self.ayear = ayear
+        self.address2 = address2
+        self.phone2 = phone2
+        self.notes = notes
