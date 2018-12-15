ee4f09f62f50caa9aa8c432292802c519ff02001
elenaerdman <lenauttwin@gmail.com>
2018-12-16 00:28:48 +0300

    Перенос wd = self.wd во вспомогательные функции

--- test_add_contact.py
+++ test_add_contact.py
@@ -11,10 +11,12 @@ class TestAddContact(unittest.TestCase):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
 
-    def open_home_page(self, wd):
+    def open_home_page(self):
+        wd = self.wd
         wd.get("http://localhost/addressbook/")
 
-    def login(self, wd, username, password):
+    def login(self, username, password):
+        wd = self.wd
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys(username)
@@ -22,10 +24,12 @@ class TestAddContact(unittest.TestCase):
         wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//input[@value='Login']").click()
 
-    def open_contacts_page(self, wd):
+    def open_contacts_page(self):
+        wd = self.wd
         wd.find_element_by_link_text("add new").click()
 
-    def create_contact(self, wd, contact):
+    def create_contact(self, contact):
+        wd = self.wd
         # fill contact form
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
@@ -90,25 +94,26 @@ class TestAddContact(unittest.TestCase):
         # submit contact creation
         wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
 
-    def return_to_home_page(self, wd):
+    def return_to_home_page(self):
+        wd = self.wd
         wd.find_element_by_link_text("home page").click()
 
-    def logout(self, wd):
+    def logout(self):
+        wd = self.wd
         wd.find_element_by_link_text("Logout").click()
     
     def test_add_contact(self):
-        wd = self.wd
         self.open_home_page(wd)
-        self.login(wd, username="admin", password="secret")
-        self.open_contacts_page(wd)
-        self.create_contact(wd, Contact(firstname="fname", middlename="mname", lastname="lname", nickname="nmane",
+        self.login(username="admin", password="secret")
+        self.open_contacts_page()
+        self.create_contact(Contact(firstname="fname", middlename="mname", lastname="lname", nickname="nmane",
                             title="123", company="inet", address="moscow, 23", hometelephone="84822343536",
                             mobiletelephone="89858883843", worktelephone="1234567", faxtelephone="12345678",
                             email="lllname@gmail.com", homepage="https://nname.com", bday="3",
                             bmonth="March", byear="1991", aday="4", amonth="May", ayear="1991",
                             address2="NY, 45", phone2="123", notes="qwerty"))
-        self.return_to_home_page(wd)
-        self.logout(wd)
+        self.return_to_home_page()
+        self.logout()
 
     def tearDown(self):
         self.wd.quit()
