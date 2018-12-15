fd63de34812206f3e6c18cff8d6166efad473ee3
elenaerdman <lenauttwin@gmail.com>
2018-12-16 00:58:52 +0300

    правка application

--- application.py
+++ application.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
 
 class Application:
-
     def __init__(self):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
--- test_add_group.py
+++ test_add_group.py
@@ -1,20 +1,15 @@
 # -*- coding: utf-8 -*-
 import pytest
-
 from group import Group
 from application import Application
 
 @pytest.fixture
 def app(request):
-    #иницилизация фикстуры
     fixture = Application()
-    #для разрушения фикстуры
     request.addfinalizer(fixture.destroy)
     return fixture
 
-#тестовый метод принимающий в качестве параметра фикстуру
 def test_add_group(app):
-    #вызывающие в фикстуре вспомогательные методы
     app.login(username="admin", password="secret")
     app.create_group(Group(name="qwerty1", header="qwerty1", footer="qwerty1"))
     app.logout()
