9f9a12ad2358ea936c6f75b6a90a3e783e7254c6
elenaerdman <lenauttwin@gmail.com>
2018-12-16 11:28:24 +0300

    выделен класс для управления сессиями для контактов

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -12,11 +12,11 @@ def app(request):
 
 def test_add_contact(app):
     app.session.login(username="admin", password="secret")
-    app.create_contact(Contact(firstname="fname", middlename="mname", lastname="lname", nickname="nmane",
-                            title="123", company="inet", address="moscow, 23", hometelephone="84822343536",
-                            mobiletelephone="89858883843", worktelephone="1234567", faxtelephone="12345678",
-                            email="lllname@gmail.com", homepage="https://nname.com", bday="3",
-                            bmonth="March", byear="1991", aday="4", amonth="May", ayear="1991",
-                            address2="NY, 45", phone2="123", notes="qwerty"))
+    app.contact.create(Contact(firstname="fname", middlename="mname", lastname="lname", nickname="nmane",
+                               title="123", company="inet", address="moscow, 23", hometelephone="84822343536",
+                               mobiletelephone="89858883843", worktelephone="1234567", faxtelephone="12345678",
+                               email="lllname@gmail.com", homepage="https://nname.com", bday="3",
+                               bmonth="March", byear="1991", aday="4", amonth="May", ayear="1991",
+                               address2="NY, 45", phone2="123", notes="qwerty"))
     app.session.logout()
 
