af83a40522db6f37604305e479178fb28b94a821
elenaerdman <lenauttwin@gmail.com>
2018-12-16 11:14:10 +0300

    выделен класс для управления сессиями

---
+++ test/test_add_contact.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+import pytest
+
+from Model.contact import Contact
+from fixture.application import Application
+
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
+
+def test_add_contact(app):
+    app.session.login(username="admin", password="secret")
+    app.create_contact(Contact(firstname="fname", middlename="mname", lastname="lname", nickname="nmane",
+                            title="123", company="inet", address="moscow, 23", hometelephone="84822343536",
+                            mobiletelephone="89858883843", worktelephone="1234567", faxtelephone="12345678",
+                            email="lllname@gmail.com", homepage="https://nname.com", bday="3",
+                            bmonth="March", byear="1991", aday="4", amonth="May", ayear="1991",
+                            address2="NY, 45", phone2="123", notes="qwerty"))
+    app.session.logout()
+
