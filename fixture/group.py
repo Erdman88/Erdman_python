6848b1307f1e61d805592f9c101d44378a4f99c7
elenaerdman <lenauttwin@gmail.com>
2019-01-12 23:13:18 +0300

    1. Исправлена ошибка при добавлении/редактировании групп/контактов (очищение редактируемых параметров) 2. Добавлен рандом в тестах для групп

--- fixture/group.py
+++ fixture/group.py
@@ -31,7 +31,7 @@ class GroupHelper:
         if text is not None:
             wd.find_element_by_name(field_name).send_keys(text)
             wd.find_element_by_name(field_name).click()
-            wd.find_element_by_name(field_name).clear()
+            #wd.find_element_by_name(field_name).clear()
 
     def modify_first_group(self, new_group_data):
         wd = self.app.wd
@@ -46,18 +46,21 @@ class GroupHelper:
         self.return_to_groups_page()
         self.group_cache = None
 
-    def click_selected(self):
+    def select_group_by_index(self, index):
         wd = self.app.wd
-        wd.find_element_by_name("selected[]").click()
+        wd.find_elements_by_name("selected[]")[index].click()
 
-    def delete_first_group(self):
+    def delete_group_by_index(self, index):
         wd = self.app.wd
         self.open_groups_page()
-        self.click_selected()
+        self.select_group_by_index(index)
         # submit deletion
         wd.find_element_by_name("delete").click()
         self.return_to_groups_page()
         self.group_cache = None
+
+    def delete_first_group(self):
+        self.delete_group_by_index(0)
 
     def select_first_group(self):
         wd = self.app.wd
@@ -65,9 +68,12 @@ class GroupHelper:
         wd.find_element_by_name("group_name").clear()
 
     def edit_first_group(self):
+        self.edit_group_by_index(0)
+
+    def edit_group_by_index(self, index):
         wd = self.app.wd
         self.open_groups_page()
-        self.click_selected()
+        self.select_group_by_index(index)
         # submit edit
         wd.find_element_by_name("edit").click()
         # change group name
--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -1,9 +1,12 @@
 from model.group import Group
+from random import randrange
 
-def test_edit_first_group(app):
+def test_edit_some_group(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
     old_groups = app.group.get_group_list()
-    app.group.edit_first_group()
+    index = randrange(len(old_groups))
+    app.group.edit_group_by_index(index)
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
+    old_groups[index:index + 1] = []
