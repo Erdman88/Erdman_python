92b3ba39bf48b43d9f141d6693348778266139b3
EhrdmanEV <Elena.Ehrdman@moex.com>
2019-02-05 09:23:37 +0300

    Добавлен конфигурационный файл для описания: target.json

--- conftest.py
+++ conftest.py
@@ -3,29 +3,30 @@ import pytest
 from fixture.application import Application
 
 fixture = None
+target = None
 
 @pytest.fixture
 def app(request):
     global fixture
-    if fixture is None:
+    global target
+    if target is None:
+        with open(request.config.getoption("--target")) as config_file:
+            target = json.load(config_file)
+    if fixture is None or not fixture.is_valid():
         browser = request.config.getoption("--browser")
-        base_url = request.config.getoption("--baseUrl")
-        fixture = Application(browser=browser, base_url=base_url)
-        fixture.session.login(username="admin", password="secret")
-    else:
-        if not fixture.is_valid():
-            fixture = Application()
-            fixture.session.login(username="admin", password="secret")
+        #base_url = request.config.getoption("--baseUrl")
+        fixture = Application(browser=browser, base_url=target["baseUrl"])
+        fixture.session.ensure_login(username=target["username"], password=target["password"])
     return fixture
 
 @pytest.fixture(scope="session", autouse=True)
 def stop(request):
     def fin():
-        fixture.session.logout()
+        fixture.session.ensure_logout()
         fixture.destroy()
     request.addfinalizer(fin)
     return fixture
 
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default = "firefox")
-    parser.addoption("--baseUrl", action="store", default="http://localhost/addressbook/index.php")
+    parser.addoption("--target", action="store", default="target.json")
