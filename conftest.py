188e94a636d9033d43b2795a3e0f2c4ed72db06b
EhrdmanEV <Elena.Ehrdman@moex.com>
2019-02-05 10:13:36 +0300

    Реализовано: загрузка информации о тестируемом приложении из конфигурационного файла

--- conftest.py
+++ conftest.py
@@ -1,4 +1,5 @@
 import pytest
+import os.path
 
 from fixture.application import Application
 
@@ -10,13 +11,14 @@ def app(request):
     global fixture
     global target
     if target is None:
-        with open(request.config.getoption("--target")) as config_file:
-            target = json.load(config_file)
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption("--target"))
+        with open(config_file) as f:
+            target = json.load(f)
     if fixture is None or not fixture.is_valid():
         browser = request.config.getoption("--browser")
         #base_url = request.config.getoption("--baseUrl")
         fixture = Application(browser=browser, base_url=target["baseUrl"])
-        fixture.session.ensure_login(username=target["username"], password=target["password"])
+    fixture.session.ensure_login(username=target["username"], password=target["password"])
     return fixture
 
 @pytest.fixture(scope="session", autouse=True)
