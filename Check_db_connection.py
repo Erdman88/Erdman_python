df09ba94588cb5bf031088ceeb837ef02ade7771
EhrdmanEV <Elena.Ehrdman@moex.com>
2019-02-13 09:21:10 +0300

    Ограничения или условия отбора загружаемых данных

--- Check_db_connection.py
+++ Check_db_connection.py
@@ -1,11 +1,11 @@
-import pymysql.cursor
+from fixture.orm import ORMFixture
 
-connection = pymysql.connector.connect(host="127.0.0.1", database="addressbook", user="root", password="root")
+db = ORMFixture(host="127.0.0.1", name="addressbook", user="root", password="root")
 
 try:
-    cursor = connection.cursor()
-    cursor.execute("select * from group_list")
-    for row in cursor.fetchall():
-        print(row)
+    l = db.get_group_list()
+    for item in l:
+        print(item)
+    print(len(l))
 finally:
-    connection.close()
+    pass #db.destroy()
