cb15b7ac84c2bdc459ecd7d58722d752816c2db4
EhrdmanEV <Elena.Ehrdman@moex.com>
2019-02-13 09:45:23 +0300

    Отношения между объектами

--- fixture/orm.py
+++ fixture/orm.py
@@ -13,6 +13,7 @@ class ORMFixture:
         name = Optional(str, column='group_name')
         header = Optional(str, column='group_header')
         footer = Optional(str, column='group_footer')
+        contacts = Set(lambda: ORMFixture.ORMContact, table="address_in_groups", column="id", reverse="groups", lazy=True)
 
     class ORMContact(db.Entity):
         _table_ = 'addressbook'
@@ -20,10 +21,11 @@ class ORMFixture:
         firstname = Optional(str, column='firstname')
         lastname = Optional(str, column='lastname')
         deprecated = Optional(datetime, column='deprecated')
+        groups = Set(lambda: ORMFixture.ORMGroup, table="address_in_groups", column="group_id")
 
     def __init__(self, hast, name, user, password):
         #в conv передается набор конвертеров-преобразователей для отдельных типов данных
-        self.db.bind('mysql', host=host, database=name, user=user, password=password, conv=decoders)
+        self.db.bind('mysql', host=host, database=name, user=user, password=password, conv=decoders, reverse="groups", lazy=True)
         #сопоставление свойств, описанных в классах с таблицами и полями этих таблиц
         self.db.ganerate_mapping()
         sql_debug(True)
@@ -49,3 +51,16 @@ class ORMFixture:
         #Извлекаем объект из БД с помощью select
         #преобразуем в список
         return self.convert_contacts_to_model(select(c for c in ORMFixture.ORMContact if c.deprecated is None))
+
+    @db_session
+    def get_contacts_in_group(self, group):
+        orm_group = list(select(g for g in ORMFixture.ORMGroup if g.id == group.id))[0]
+        return self.convert_contacts_to_model(orm_group.contacts)
+
+    @db_session
+    def get_contacts_not_in_group(self, group):
+        orm_group = list(select(g for g in ORMFixture.ORMGroup if g.id == group.id))[0]
+        return self.convert_contacts_to_model(
+            select(c for c in ORMFixture.ORMContact if c.deprecated is None and orm_group not in c.groups))
+
+
